14dd5947f58a99fd0209963584e208c98bc84b00
joyce <jycyeo@yahoo.com.sg>
2022-09-01 02:28:41 +0800

    initial commit

---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
+  
+import Image from "react-bootstrap/Image";
+
+export default function Header() {
+  return (
+      <Image src="https://github.com/pikulet/roolet/blob/master/src/images/roolet.png" rounded/>
+  );
+}
