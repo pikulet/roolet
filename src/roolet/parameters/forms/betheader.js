985d210a7d3c8598d27ba04ed252c3127e0b94d6
joyce <jycyeo@yahoo.com.sg>
2022-09-01 17:07:01 +0800

    update pretty config

--- src/roolet/parameters/forms/betheader.js
+++ src/roolet/parameters/forms/betheader.js
@@ -1,18 +1,18 @@
-import React from 'react';
+import React from 'react'
 
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class BetHeader extends React.Component {
-    render() {
-        return (
-            <Row>
-                <Col md={4}>Pr</Col>
-                <Col md={4}>X</Col>
-                <Col md={4}>$</Col>
-            </Row>
-        );
-    }
+  render() {
+    return (
+      <Row>
+        <Col md={4}>Pr</Col>
+        <Col md={4}>X</Col>
+        <Col md={4}>$</Col>
+      </Row>
+    )
+  }
 }
 
-export default BetHeader;
+export default BetHeader
--- src/roolet/summary/summary.js
+++ src/roolet/summary/summary.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React from 'react'
 
 class Summary extends React.Component {
-    render() {
-        return <h1>summary</h1>;
-    }
+  render() {
+    return <h1>summary</h1>
+  }
 }
 
-export default Summary;
+export default Summary
